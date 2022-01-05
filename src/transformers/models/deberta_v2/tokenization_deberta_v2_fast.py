8db4adfba4ac482582239e1d59ef583b6b7b3dc4
SaulLu <lucilesaul.com@gmail.com>
2022-01-05 19:04:39 +0100

    update `VOCAB_FILES_NAMES`

--- src/transformers/models/deberta_v2/tokenization_deberta_v2_fast.py
+++ src/transformers/models/deberta_v2/tokenization_deberta_v2_fast.py
@@ -30,6 +30,8 @@ else:
 
 logger = logging.get_logger(__name__)
 
+VOCAB_FILES_NAMES = {"vocab_file": "spm.model", "tokenizer_file": "tokenizer.json"}
+
 PRETRAINED_VOCAB_FILES_MAP = {
     "vocab_file": {
         "microsoft/deberta-v2-xlarge": "https://huggingface.co/microsoft/deberta-v2-xlarge/resolve/main/spm.model",
@@ -52,8 +54,6 @@ PRETRAINED_INIT_CONFIGURATION = {
     "microsoft/deberta-v2-xlarge-mnli": {"do_lower_case": False},
     "microsoft/deberta-v2-xxlarge-mnli": {"do_lower_case": False},
 }
-
-VOCAB_FILES_NAMES = {"vocab_file": "spm.model"}
 
 
 class DebertaV2TokenizerFast(PreTrainedTokenizerFast):
